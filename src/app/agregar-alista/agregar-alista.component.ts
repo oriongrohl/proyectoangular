1543fc22a788f9412a51b7036894cc14fe25f1ed
JesMM9 <jes9mimi9@gmail.com>
2025-06-20 11:47:23 +0200

    Añadido el componente del botón agregar

---
+++ src/app/agregar-alista/agregar-alista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-alista',
+  standalone: true,
+  imports: [],
+  templateUrl: './agregar-alista.component.html',
+  styleUrl: './agregar-alista.component.css'
+})
+export class AgregarAListaComponent {
+  
+}
